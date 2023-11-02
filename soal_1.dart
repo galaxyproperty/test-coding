25c3e0355656be9b5b9e590c82de454cb057e1bb
mambaur <alwiroziq1998@gmail.com>
2023-11-02 14:44:38 +0700

    first commit

---
+++ soal_1.dart
@@ -0,0 +1,20 @@
+/// Tugas 1
+///
+/// Buatlah fungsi dengan nama calculatePow yang akan menghitung
+/// hasil dari x pangkat n.
+///
+/// Fungsi ini harus mengembalikan hasil perhitungan.
+
+double calculatePow(double x, int n) {
+  // Implementasikan fungsi ini
+  // ...
+}
+
+void main() {
+  double x = 2.0;
+  int n = 3;
+  double result = calculatePow(x, n);
+  print('$x pangkat $n: $result');
+
+  // Output yang diharapkan: 8.0
+}
---
+++ soal_2.dart
@@ -0,0 +1,23 @@
+/// Tugas 2
+///
+/// Buatlah kelas `Animal` yang akan mewakili hewan.
+/// Kelas ini harus memiliki properti berikut:
+/// - name (String): Nama hewan.
+/// - species (String): Jenis hewan.
+///
+/// Selanjutnya, buat dua subclass dari Animal yaitu `Mammal` dan `Bird`.
+/// Mammal akan mewakili hewan-hewan bertipe mamalia,
+/// sementara Bird akan mewakili hewan-hewan bertipe burung.
+///
+/// Setiap subclass harus memiliki properti yang sesuai,
+/// Misalnya, Mammal dapat memiliki properti `furColor`
+/// dan Bird dapat memiliki properti `wingLength`.
+
+void main() {
+  Mammal mammal = Mammal(name: "Elephant");
+  print(mammal.name);
+  // Outputnya harus menampilkan "Elephant"
+}
+
+// Buat kelasnya dibawah ini
+// ...
